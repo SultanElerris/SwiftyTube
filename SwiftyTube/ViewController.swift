e23423745da361931988baca4d6a7b9f5a92bc3f
Sultan <srs389@psu.edu>
2017-01-08 19:03:43 -0500

    Initial Commit

---
+++ SwiftyTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyTube
+//
+//  Created by Sultan Sultan on 1/8/17.
+//  Copyright © 2017 Sultan Sultan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
