245b7136ccb6e76dcaab76b582c11e3dc954bc03
Sultan <srs389@psu.edu>
2017-01-22 13:59:36 -0500

    Added the image, title, subtitle, user image subview to the cell.

--- SwiftyTube/ViewController.swift
+++ SwiftyTube/ViewController.swift
@@ -17,7 +17,7 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         
         collectionView?.backgroundColor = UIColor.white
         
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "SwiftyTubeCell")
+        collectionView?.register(videoCell.self, forCellWithReuseIdentifier: "SwiftyTubeCell")
         
     }
 
@@ -34,9 +34,7 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SwiftyTubeCell", for: indexPath)
-        
-        cell.backgroundColor = UIColor.red
-        
+
         return cell
     }
     
@@ -46,7 +44,134 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 250)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+class videoCell:UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    // Thumbnail Image View (Lazy Property)
+    let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.lightGray
+        return imageView
+    }()
+    
+    // Separator (Lazy Property)
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        return view
+    }()
+    
+    let userProfileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.orange
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = UIColor.purple
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+        
+    }()
+    
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = UIColor.red
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+        
+    }()
+    
+    
+    // View Setup
+    func setupViews() {
+        
+        
+        //Thumbnail
+        addSubview(thumbnailImageView)
+        // Separator
+        addSubview(separatorView)
+        
+        // User Profile
+        addSubview(userProfileImageView)
+        
+        addSubview(titleLabel)
+        
+        addSubview(subtitleLabel)
+        
+        addConstrantsWithFormat(format: "H:|-16-[v0]-16-|", views: thumbnailImageView)
+        
+        
+        addConstrantsWithFormat(format: "H:|-16-[v0(44)]", views: userProfileImageView)
+        
+        
+        addConstrantsWithFormat(format: "V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|", views: thumbnailImageView,userProfileImageView ,separatorView)
+      
+        
+        addConstrantsWithFormat(format: "H:|[v0]|", views: separatorView)
+        
+        //left constrant
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: userProfileImageView, attribute: .right, multiplier: 1, constant: 8))
+        
+        
+        //top constrant 
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: thumbnailImageView, attribute: .bottom, multiplier: 1, constant: 8))
+        
+        //right constrant 
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+        
+        // Height
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+        
+        //Subtitle -----------
+        
+        //left constrant
+        addConstraint(NSLayoutConstraint(item: subtitleLabel, attribute: .left, relatedBy: .equal, toItem: userProfileImageView, attribute: .right, multiplier: 1, constant: 8))
+        
+        
+        //top constrant
+        addConstraint(NSLayoutConstraint(item: subtitleLabel, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1, constant: 4))
+        
+        //right constrant
+        addConstraint(NSLayoutConstraint(item: subtitleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+        
+        // Height
+        addConstraint(NSLayoutConstraint(item: subtitleLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+        
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
 
+extension UIView {
+
+    func addConstrantsWithFormat(format: String, views: UIView...) {
+        
+        
+        var viewDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewDictionary))
+    }
 }
 
